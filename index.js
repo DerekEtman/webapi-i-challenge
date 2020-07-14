e5b371dd9e6fdaa3ed9a208d22ecb8622850de88
Derek Etman <dereketman@gmail.com>
2020-07-13 21:11:45 -0700

    That was fun.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+server.listen(8000, () => console.log("API running on port 8000"));
+
+const hobbits = [
+	{
+		id: 1,
+		name: "Samwise Gamgee",
+	},
+	{
+		id: 2,
+		name: "Frodo Baggins",
+	},
+];
+
+server.get("/hobbits", (req, res) => {
+	res.status(200).json(hobbits);
+});
+
+
+server.get("/names", (req, res) => {
+
+    
+    let names = hobbits.map((hobbit) => hobbit.name
+    )
+    console.log(names)
+    res.status(200).json(names)
+})
